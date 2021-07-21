bc8f725cfff1c9f8f361d47278e2c1b6d908bc2b
JPhamNguyen <jphamnguyen@scu.edu>
2021-07-21 11:43:17 -0700

    Added code to insert data by Accession Numbers

--- reformat_csv.py
+++ reformat_csv.py
@@ -8,6 +8,7 @@ Returns a saved copy of the newly reformatted CSV file, it does not alter the or
 
 import csv
 import pandas as pd
+import numpy as np
 import os
 import sys
 
@@ -46,36 +47,78 @@ def add_columns_from_other_file(other_filename, columns, df):
     return reformatted_df
 
 
-def add_data_with_labels(df, input, labels):
+def add_data_by_accession_number(df, input_data):
     """Insert data from one dataframe input to another based on the associated label with that data
+    It inserts data by Accession Number, and both the df and the input should have their respective associated
+    'Accession Number' data, so that this code can filter and insert data correctly
     Args:
         :param: df (pandas DataFrame): the dataset which we want to insert new data into
-        :param: input (pandas Series or Dataframe): the data to be inputted
-        :param: labels (pandas Series): the labels by which we sort and then insert the data
+        :param: input (pandas Series or Dataframe): the data we want to input into the dataset
+    Returns: the new 'df' with the appended data from 'input'
     """
-    # pseudo code
-    # for label in labels:
-    #     for feature in input.columns:
-    #          for i in range(len(input[feature]):
-    #               # input data based on the label
-    #               if KeyError:
-    #                   print("An Accession Number is incorrect")
-    if isinstance(input, pd.DataFrame):
-        columns = list(input.columns)
-    else:
-        columns = input.name
+    # set the accession numbers to indexes to use for filtering
+    df.set_index('Accession Number', inplace=True)
+    input_data.set_index('Accession Number', inplace=True)
 
-    # access by each Accession Number
-    for label in labels:
-        # access by each data feature in the input's column(s)
+    # obtain the feature(s) whose data is/are being inputted
+    if isinstance(input_data, pd.DataFrame):
+        columns = list(input_data.columns)
+    else:
+        columns = [input_data.name]
+
+    for col in columns:
+        df[col] = np.nan
+
+    # loop through the Accession Numbers of the dataset receiving the data, and insert data after filtering
+    # by Accession Number
+    key_errors = []
+    for accession_number in df.index:
         for feature in columns:
-            # for each value within that data feature column
-            for i, val in enumerate(input[feature]):
-                print("nice")
-                # have to figure out how to filter by the Accession # and then the 
+            try:
+                df.at[accession_number, feature] = input_data.loc[accession_number, feature]
+            except KeyError:
+                if accession_number not in key_errors:
+                    key_errors.append(accession_number)
+                pass
+
+    # print information about any possible missing Accession Numbers from input_data
+    print("Number of missing Accession Numbers: \n{}\n".format(len(key_errors)))
+    print("List of missing Accession Numbers themselves: \n{}\n".format(key_errors))
+
+    # reset the indexing of the dataset and return final DataFrame
+    df.reset_index(inplace=True)
+    return df
+
+
+def remove_strings(df, column, str_to_delete):
+    """Strips any unnecessary characters/strings from a column(s)
+    of data, such as '%', or 'ph', and etc., and returns just the data itself
+    Args:
+        :param: df (Pandas DataFrame): this is the df containing the column to edit
+        :param: column (string): the column to edit
+        :param: str_to_delete: string or character to delete from the column
+    Returns:
+        :return: df (Pandas DataFrame): the dataframe containing the column with the str/char removed
+    """
+    df[column] = df[column].map(lambda x: x.lstrip(str_to_delete).rstrip(str_to_delete))
+    return df
+
+
+def save_to_csv(df, pathway):
+    """For convenience, save the DataFrame as a CSV file into whatever pathway is designated
+    Args:
+        :param: df (Pandas DataFrame): the dataframe that we want to save as a CSV file
+        :param: pathway (string): a string defining the file pathway where the CSV file is saved
+    Returns: None
+    """
+    df.to_csv(pathway)
 
 
 if __name__ == '__main__':
+    # note for future self:
+    # might want to type in code to receive input on which commands and what you want to edit
+    # this will probably take some time to code up
+
     # This bit of code reformats the dataset to delete and add columns, but this section of code is
     # inflexible since every single command must be hardcoded (will require future revision for ease of use)
     # assert len(sys.argv) == 2, "First command line argument is name of the new CSV database file,"
@@ -98,10 +141,19 @@ if __name__ == '__main__':
     # dataset.to_csv(path_or_buf=output_file, header=list(dataset.columns.values), index=False)
     # print("Successfully reformatted CSV database file\n")
 
-    # This could be an automated function right where I pass in the name of the file
-    gravy_scores = pd.read_csv('Reformatted_Files/GRAVY_Scores.csv')
-    add_data_with_labels(pd.read_csv('Reformatted_Files/_new_database.csv'), gravy_scores[['GRAVY', '% h-bonding']],
-                         gravy_scores['Accession Number'])
+    pd.set_option('display.max_rows', None, 'display.max_columns', None)
+
+    # read in the CSV file with the new features and remove the '%' from h-bonding column
+    new_features = pd.read_csv('Reformatted_Files/New_Features.csv')
+    new_features = remove_strings(new_features, '% H-bond/polar AA', '%')
+
+    # adds data according to accession numbers (GRAVY scores + % h-bonding)
+    original_dataset = pd.read_csv('Reformatted_Files/_new_database.csv')
+    new_df = add_data_by_accession_number(original_dataset, new_features)
+    new_df.to_csv('Reformatted_Files/_updated_database.csv')
+
+
+
 
 
 
