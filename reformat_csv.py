2bec1acdf2dc3fe568a0e7a39019eebf93d88f08
Joseph Pham Nguyen <jphamnguyen@scu.edu>
2021-08-26 15:32:26 -0700

    Added pandas manipulation of original Excel file for data insertion

--- reformat_csv.py
+++ reformat_csv.py
@@ -134,5 +134,162 @@ def save_to_csv(df, pathway):
     df.to_csv(pathway, index=False)
 
 
-if __name__ == '__main__':
+def clean_original_data():
+    """ Due to the nested structure of the original Excel file, reformat the original Excel file into a more easily
+    manipulated dataframe that can be used for other operations like inserting new data by accession number and etc.
+
+    Args: None
+    Returns:None
+    """
+    # create 2 different DataFrames to read and parse different headers
+    info1 = pd.read_csv('Input_Files/information.csv', header=[0])
+    info2 = pd.read_csv('Input_Files/information.csv', header=[1])
+
+    # drop undesired columns, and preserve condition-independent data into the index
+    cols_to_drop = ['identified protein, description', 'gene names', 'Gene Ontology, Biological Process ',
+                    'Gene Ontology, Cellular Component', 'Gene Ontology, Molecular Function', 'Unnamed: 40']
+    info2.drop(labels=cols_to_drop, axis=1, inplace=True)
+    info2.set_index(keys=['accession number', 'pI', 'length', 'molecular weight', 'Enzyme Commission number'], inplace=True)
+
+    # get the various conditions from info1
+    conditions = []
+    for col in info1.columns:
+        if col.find('Unnamed:') == -1:
+            conditions.append(col)
+
+    condition_arr = []
+    nest = []
+    # to create the multi-index, loop through the columns and create the arrays needed to create MultiIndex
+    for idx, label in enumerate(info2.columns):
+        label += '.' + str(idx)
+        split_label = label.split(sep='.')
+        del split_label[len(split_label) - 1]
+
+        if len(split_label) > 1:
+            char = split_label[len(split_label) - 1]
+            condition = conditions[int(char)]
+            nest.append(''.join(split_label[0:len(split_label) - 1]))
+        else:
+            condition = conditions[0]
+            nest.append(''.join(split_label))
+        condition_arr.append(condition)
+
+    # get rid of any whitespace for nicer formatting
+    for i in range(len(nest)):
+        nest[i] = nest[i].rstrip()
+
+    # create a new DataFrame with a proper MultiIndex
+    arrays = [condition_arr, nest]
+    tuples = list(zip(*arrays))
+    multi_index = pd.MultiIndex.from_tuples(tuples, names=['conditions', 'values'])
+    df = pd.DataFrame(data=info2.values, index=info2.index, columns=multi_index)
+
+    # drop nested non-Bound-Fraction columns
+    df.drop(labels=['Bound Fraction, standard deviation in NSAF value', 'Unbound Fraction, average NSAF value'
+                    , 'Unbound Fraction, average NSAF value', 'Unbound Fraction, standard deviation in NSAF value'
+                    , 'Enrichment Factor'], axis=1, level='values', inplace=True)
+
+    # replace the '---'/0.00E+00 values with 0 in the DataFrame
+    df.replace(to_replace=['---', '0.00E+00'], value=0, inplace=True)
+    df.reset_index(level=['pI', 'length', 'molecular weight', 'Enzyme Commission number'], inplace=True)
+    df = df.apply(pd.to_numeric, errors='coerce')
+
+    # replace 0 values for molecular weight
+    df['molecular weight'] = df['molecular weight'].replace(to_replace=0.00, value=np.nan)
+
+    insert_accession_numbers(df)
+
+
+def insert_accession_numbers(inputted_data):
+    """ Insert relevant data by accession number into the modeling dataset
+
+    Args:
+        pandas DataFrame inputted_data: the reformatted input DataFrame to be used for inserting data into the modeling dataset
+    Returns: At the moment, None
+    """
+    dataset = pd.read_csv('Input_Files/database.csv')
+    sliced_input_data = inputted_data.xs('Bound Fraction, average NSAF value', level='values', axis=1)
+    conditions_list = list(sliced_input_data.columns)
+
+    missing_accession_numbers = set()
+    incomplete_accession_numbers = set()
+    occurrences_of_accession_numbers = {}
+
+    # find accession numbers excluded from modeling dataset
+    access_nums = list(dataset['Accession Number'])
+    for access_num in list(sliced_input_data.index):
+        if access_num not in access_nums:
+            missing_accession_numbers.add(access_num)
+
+    # find list of incomplete accession numbers
+    for a_num in access_nums:
+        if a_num not in occurrences_of_accession_numbers:
+            occurrences_of_accession_numbers[a_num] = 1
+        else:
+            occurrences_of_accession_numbers[a_num] += 1
+
+    # filter for incomplete Accession Numbers
+    for key in occurrences_of_accession_numbers.keys():
+        if occurrences_of_accession_numbers[key] < 6:
+            incomplete_accession_numbers.add(key)
+
+    print(f'Number of missing accession numbers: {len(missing_accession_numbers)}\n')
+    print(f'Number of incomplete accession numbers: {len(incomplete_accession_numbers)}\n')
+
+    # insert the Bound Fraction values for the missing accession numbers
+    missing_a_num_df = pd.DataFrame(columns=dataset.columns)
+
+    # loop through all of the accession numbers in the inputted dataset
+    for condition in conditions_list:
+        # filter for particle properties
+        split_label = condition.split()
+        if split_label[0].find('(+)') != -1:
+            particle_charge = 1
+        else:
+            particle_charge = 0
+
+        if split_label[1] == '10':
+            particle_size = 10
+        else:
+            particle_size = 100
+
+        # filter for solvent conditions
+        if len(split_label) <= 9:
+            cys_concentration = nacl_concentration = 0
+        else:
+            # check for cysteine or sodium chloride concentration
+            if split_label[12] == 'NaCl':
+                nacl_concentration = split_label[10]
+                cys_concentration = 0
+            else:
+                cys_concentration = split_label[10]
+                nacl_concentration = 0
+        for a_num in missing_accession_numbers:
+            missing_a_num_df.at[len(missing_a_num_df.index), 'Particle Charge'] = particle_charge
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Particle Size'] = particle_size
+
+            # inserting solvent + concentration
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Solvent NaCl Concentration'] = nacl_concentration
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Solvent Cysteine Concentration'] = cys_concentration
+
+            # insert other relevant data
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Accession Number'] = a_num
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Bound Fraction'] = sliced_input_data.at[a_num, condition]
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Bound Fraction'] = sliced_input_data.at[a_num, condition]
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Enzyme Commission Number'] = inputted_data.loc[a_num, 'Enzyme Commission number'].iat[0]
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'pI'] = inputted_data.loc[a_num, 'pI'].iat[0]
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Protein Length'] = inputted_data.loc[a_num, 'length'].iat[0]
+            missing_a_num_df.at[len(missing_a_num_df.index) - 1, 'Protein Weight'] = inputted_data.loc[a_num, 'molecular weight'].iat[0]
+
+
+def find_bound_fraction_filler():
+    # needs to be developed to find the filler value for Bound Fraction since it's more representative of the actual state of the modeling dataset
     pass
+
+
+if __name__ == '__main__':
+    info = pd.read_csv('Input_Files/information.csv', header=[0])
+
+    # pd.set_option('display.max_columns', None, 'display.max_rows', None)
+    clean_original_data()
+
